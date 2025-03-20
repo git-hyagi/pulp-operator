8b02ad4442f0fd5b347dc20064a05aff4ea799d0
git-hyagi <45576767+git-hyagi@users.noreply.github.com>
2025-03-20 19:10:43 -0300

    draft migrate galaxy ci tests

--- .ci/scripts/backup_and_restore.sh
+++ .ci/scripts/backup_and_restore.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 echo "Set context"
 kubectl config set-context --current --namespace=pulp-operator-system
 
-BACKUP_RESOURCE=repo-manager_v1beta2_pulpbackup.yaml
-RESTORE_RESOURCE=repo-manager_v1beta2_pulprestore.yaml
+BACKUP_RESOURCE=repo-manager.pulpproject.org_v1_pulpbackup.yaml
+RESTORE_RESOURCE=repo-manager.pulpproject.org_v1_pulprestore.yaml
 
 if [[ "$CI_TEST" == "true" ]]; then
   CUSTOM_RESOURCE=simple.yaml
@@ -13,8 +13,8 @@ elif [[ "$CI_TEST" == "galaxy" && "$CI_TEST_STORAGE" == "filesystem" ]]; then
   CUSTOM_RESOURCE=galaxy.yaml
 elif [[ "$CI_TEST" == "galaxy" && "$CI_TEST_STORAGE" == "azure" ]]; then
   CUSTOM_RESOURCE=galaxy.azure.ci.yaml
-elif [[ "$CI_TEST" == "galaxy" && "$CI_TEST_STORAGE" == "s3" ]]; then
-  CUSTOM_RESOURCE=galaxy.s3.ci.yaml
+elif [[ "$CI_TEST_STORAGE" == "s3" ]]; then
+  CUSTOM_RESOURCE=simple.s3.ci.yaml
 fi
 
 echo ::group::PRE_BACKUP_LOGS
@@ -36,7 +36,7 @@ kubectl delete -f config/samples/$CUSTOM_RESOURCE
 # deleting resources that have no operator owerReference to better validate that
 # restore controller will recreate them instead of "reusing" the older ones
 kubectl delete secrets --all
-kubectl delete pvc -l pulp_cr=galaxy-example
+kubectl delete pvc -l pulp_cr=test-pulp
 kubectl wait --for=delete --timeout=300s -f config/samples/$CUSTOM_RESOURCE
 
 kubectl apply -f config/samples/$RESTORE_RESOURCE
@@ -48,7 +48,7 @@ kubectl logs -l app.kubernetes.io/name=pulp-operator -c manager --tail=10000
 echo ::endgroup::
 
 sudo pkill -f "port-forward" || true
-time kubectl wait --for condition=Galaxy-Operator-Finished-Execution galaxy.repo-manager.ansible.com/galaxy-example --timeout=800s
+time kubectl wait --for condition=Pulp-Operator-Finished-Execution pulp.repo-manager.pulpproject.org/test-pulp --timeout=800s
 kubectl get pods -o wide
 
 KUBE="k3s"
--- .ci/scripts/prepare-object-storage.sh
+++ .ci/scripts/prepare-object-storage.sh
@@ -16,7 +16,7 @@ elif [[ "$CI_TEST_STORAGE" == "s3" ]]; then
   sudo chmod +x /usr/local/bin/mc
   while ! nc -z $(minikube ip) 9000; do echo 'Wait minio to startup...' && sleep 0.1; done;
   echo $(minikube ip)   pulp_minio | sudo tee -a /etc/hosts
-  sed -i "s/pulp_minio/$(minikube ip)/g" config/samples/galaxy.s3.ci.yaml
+  sed -i "s/pulp_minio/$(minikube ip)/g" config/samples/simple.s3.ci.yaml
   mc config host add s3 http://$(minikube ip):9000 AKIAIT2Z5TDYPX3ARJBA fqRvjWaPU5o0fCqQuUWbj9Fainj2pVZtBCiDiieS --api S3v4
   mc config host rm local
   mc mb s3/pulp3 --region us-east-1
